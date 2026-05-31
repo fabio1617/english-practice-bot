01310543754a1ad22701fd0e53e5cbeae42d2ece
fabio1617 <fabio.1617@hotmail.com>
2026-05-31 11:27:37 -0300

    Melhora layout da sidebar e fluxo de aulas

--- backend/perguntas.py
+++ backend/perguntas.py
@@ -297,7 +297,363 @@ CENARIOS = {
         {
             'pergunta': "Boa tarde! O que vocês preferem para o almoço?",
             'respostas': ['Good afternoon! What do you prefer for lunch?']
+        },
+        {
+            'pergunta': "Eu prefiro salada e ela prefere peixe.",
+            'respostas': ['I prefer salad and she prefers fish.']
+        },
+        {
+            'pergunta': "Eu prefiro arroz, feijão e carne no jantar, mas eles preferem salada.",
+            'respostas': ['I prefer rice, beans and meat for dinner, but they prefer salad.']
+        },
+        {
+            'pergunta': "Eu sempre cozinho massas nos finais de semana.",
+            'respostas': ['I always cook pasta on weekends.']
+        },
+        {
+            'pergunta': "Eu prefiro suco de laranja e pão de manhã.",
+            'respostas': ['I prefer orange juice and bread in the morning.']
+        },
+        {
+            'pergunta': "Ela prefere suco de maçã a suco de laranja.",
+            'respostas': ['She prefers apple juice to orange juice.']
+        },
+        {
+            'pergunta': "O que você prefere?",
+            'respostas': ['What do you prefer?']
+        },
+        {
+            'pergunta': "Eu prefiro pão, mas ela prefere frutas.",
+            'respostas': ['I prefer bread, but she prefers fruit.']
+        },
+        {
+            'pergunta': "Ele nunca cozinha em casa.",
+            'respostas': ['He never cooks at home.']
         }
     ]
-    }
-}    
+    },
+    'Aula 4 – Presente Simples\n [Pronomes objeto]': {
+        'tipo': 'aula',
+        'video': 'https://drive.google.com/file/d/1e0wUlna9hSSSoW1L3FkC0GJGAAPR_F5N/view?usp=drive_link'
+        '',
+        'perguntas': [
+        {
+            'pergunta': "Eu trabalho aqui.",
+            'respostas': ['I work here.']
+        },
+        {
+            'pergunta': "Eu não jogo futebol.",
+            'respostas': ['I do not play soccer.']
+        },
+        {
+            'pergunta': "Eu não fico em casa nos finais de semana.",
+            'respostas': ["I don’t stay at home on weekends."]
+        },
+        {
+            'pergunta': "Eu sempre limpo a casa.",
+            'respostas': ['I always clean the house.']
+        },
+        {
+            'pergunta': "Ela não cozinha comigo.",
+            'respostas': ["She doesn’t cook with me."]
+        },
+        {
+            'pergunta': "Ele prefere ficar comigo.",
+            'respostas': ['He prefers to stay with me.']
+        },
+        {
+            'pergunta': "Elas/Eles me querem aqui.",
+            'respostas': ['They want me here.']
+        },
+        {
+            'pergunta': "Ela/Ele [a cadelinha/o cãozinho] precisa de mim.",
+            'respostas': ['It needs me.']
+        },
+        {
+            'pergunta': "Ela precisa de um carro.",
+            'respostas': ['She needs a car.']
+        },
+        {
+            'pergunta': "Ela não quer um apartamento grande.",
+            'respostas': ['She does not want a big apartment.']
+        },
+        {
+            'pergunta': "Ela não prefere o carro prata.",
+            'respostas': ["She doesn’t prefer the silver car."]
+        },
+        {
+            'pergunta': "Ela nunca cozinha carne.",
+            'respostas': ['She never cooks meat.']
+        },
+        {
+            'pergunta': "Eu limpo a casa com ela.",
+            'respostas': ['I clean the house with her.']
+        },
+        {
+            'pergunta': "Nós preferimos ficar com ela.",
+            'respostas': ['We prefer to stay with her.']
+        },
+        {
+            'pergunta': "Eles não tocam violão com ela.",
+            'respostas': ["They don’t play the guitar with her"]
+        },
+        {
+            'pergunta': "Sam não trabalha com ela.",
+            'respostas': ["Sam doesn’t work with her."]
+        },
+        {
+            'pergunta': "Ele não trabalha no Brasil.",
+            'respostas': ['He plays on the computer.']
+        },
+        {
+            'pergunta': "Ele às vezes fica na Casa Branca.",
+            'respostas': ['He sometimes stays in the White House.']
+        },
+        {
+            'pergunta': "Ele não limpa o banheiro nos finais de semana.",
+            'respostas': ["He doesn’t clean the bathroom on weekends."]
+        },
+        {
+            'pergunta': "Eu sempre cozinho massas com ele.",
+            'respostas': ['I always cook pasta with him.']
+        },
+        {
+            'pergunta': "Ela não prefere jogar/tocar/brincar com ele.",
+            'respostas': ['She does not prefer to play with him.']
+        },
+        {
+            'pergunta': "Ela/Ele [a gatinha/o gatinho/ quer ele.",
+            'respostas': ['It wants him.']
+        },
+        {
+            'pergunta': "Nós precisamos dele.",
+            'respostas': ['We need him.']
+        },
+        {
+            'pergunta': "Nós não trabalhamos juntos.",
+            'respostas': ["We don’t work together."]
+        },
+        {
+            'pergunta': "Nós jogamos voleibol na Tijuca.",
+            'respostas': ['We play volleyball in Tijuca.']
+        },
+        {
+            'pergunta': "Nós às vezes ficamos em um quarto pequeno.",
+            'respostas': ['We sometimes stay in a small bedroom.']
+        },
+        {
+            'pergunta': "Nós sempre limpamos o quintal.",
+            'respostas': ['We always clean the yard.']
+        },
+        {
+            'pergunta': "Elas/Eles preferem ficar conosco. /\n Elas/Eles preferem ficar com a gente.",
+            'respostas': ['They prefer to stay with us.']
+        },
+        {
+            'pergunta': "Ele não cozinha conosco. /\n Ele não cozinha com a gente.",
+            'respostas': ['He does not cook with us.']
+        },
+        {
+            'pergunta': "Ela quer trabalhar conosco. /\n Ela quer trabalhar com a gente.",
+            'respostas': ['She wants to work with us.']
+        },
+        {
+            'pergunta': "Elas/Eles não precisam de nós. /\n Elas/Eles não precisam da gente.",
+            'respostas': ['They don’t need us.']
+        },
+        {
+            'pergunta': "Nós precisamos delas/deles.",
+            'respostas': ['We need them.']
+        },
+        {
+            'pergunta': "[A] Carla trabalha com elas/\neles nos dias de semana/dias úteis.",
+            'respostas': ['Carla works with them on weekdays.']
+        },
+        {
+            'pergunta': "[A] Lelê fica com elas/"
+            "eles nos finais de semana.",
+            'respostas': ['Lelê stays with them on weekends.']
+        },
+        {
+            'pergunta': "[O] Lucas sempre joga UNO com elas/eles.",
+            'respostas': ['Lucas always plays UNO with them']
+        },
+        {
+            'pergunta': "Elas/Eles querem jogar Mobile Legends com [o] Theobalado.",
+            'respostas': ['They want to play Mobile Legends with Theobaldo.']
+        },
+        {
+            'pergunta': "Elas/Eles não preferem o carro vermelho.",
+            'respostas': ["They don’t prefer the red car."]
+        },
+        {
+            'pergunta': "Elas/Eles sempre cozinham nos finais de semana.",
+            'respostas': ['they always cook on weekends.']
+        },
+        {
+            'pergunta': "Elas/Eles nunca limpam a cozinha.",
+            'respostas': ['They never clean the kitchen.']
+        },
+        {
+            'pergunta': "Com licença. Eu trabalho aqui.",
+            'respostas': ['Excuse me. I work here.']
+        },
+        {
+            'pergunta': "Oi! Eu prefiro o peixe. Obrigada!",
+            'respostas': ['Hi! I prefer the fish. Thank you!']
+        },
+        {
+            'pergunta': "Eu quero ficar com eles.",
+            'respostas': ['I want to stay with them.']
+        },
+        {
+            'pergunta': "Ela precisa trabalhar de manhã e eu prefiro trabalhar à tarde.",
+            'respostas': ['She needs to work in the morning and I prefer to work in the afternoon.']
+        },
+        {
+            'pergunta': "Nós não queremos uma casa grande.",
+            'respostas': ['We do not want a big house.' ,"We don't want a big house."]
+        },
+        {
+            'pergunta': "Ela quer um suco de laranja. Obrigado!",
+            'respostas': ['She wants an orange juice. Thank you!']
+        },
+        {
+            'pergunta': "Ele não quer limpar o quarto.",
+            'respostas': ['He does not want to clean the bedroom.', "He doesn't want to clean the bedroom."]
+        },
+        {
+            'pergunta': "Nós não precisamos ficar aqui. Tchau.",
+            'respostas': ['We do not need to stay here. Bye.', "We don't need to stay here. Bye."]
+        },
+        {
+            'pergunta': "Eu quero jogar com elas, mas elas não querem jogar comigo.",
+            'respostas': ['I want to play with them, but they do not want to play with me.', "I want to play with them, but they don't"
+            " want to play with me."]
+        },
+        {
+            'pergunta': "Nós precisamos trabalhar nos dias úteis, mas preferimos ficar aqui nos finais de semana.",
+            'respostas': ['We need to work on weekdays, but we prefer to stay here on weekends.']
+        },
+    ]    
+    },
+    'Aula 5 – Passado Simples': {
+        'tipo': 'aula',
+        'video': 'https://drive.google.com/file/d/1O2qVsw4Knhbp1hC78qpI6rLbcta0Ucvm/view?usp=drive_link'
+        '',
+        'perguntas': [
+        {
+            'pergunta': "Trabalhar.",
+            'respostas': ['Worked.']
+        },
+        {
+            'pergunta': "Jogar. / Brincar. / Tocar.",
+            'respostas': ['Played.']
+        },
+        {
+            'pergunta': "Ficar.",
+            'respostas': ['Stayed.']
+        },
+        {
+            'pergunta': "Limpar.",
+            'respostas': ['Cleaned.']
+        },
+        {
+            'pergunta': "Cozinhar.",
+            'respostas': ['Cookeed.']
+        },
+        {
+            'pergunta': "Preferir.",
+            'respostas': ['Preferred.']
+        },
+        {
+            'pergunta': "Querer.",
+            'respostas': ['Wanted.']
+        },
+        {
+            'pergunta': "Precisar.",
+            'respostas': ['Needed.']
+        },
+        {
+            'pergunta': "Ligar. / Chamar.",
+            'respostas': ['Called.']
+        },
+        {
+            'pergunta': "Andar. / Caminhar.",
+            'respostas': ['Walked.']
+        },
+        {
+            'pergunta': "Eu trabalho de manhã.",
+            'respostas': ['I work in the morning.']
+        },
+        {
+            'pergunta': "Eu trabalhei ontem.",
+            'respostas': ['I worked yesterday.']
+        },
+        {
+            'pergunta': "Eu jogo aqui.",
+            'respostas': ['I play here.']
+        },
+        {
+            'pergunta': "Eu jogava lá.",
+            'respostas': ['I played there.']
+        },
+        {
+            'pergunta': "Eu fico no shopping.",
+            'respostas': ['I stay in the mall.']
+        },
+        {
+            'pergunta': "Eu fiquei no escritótio.",
+            'respostas': ['I stayed in the office.']
+        },
+        {
+            'pergunta': "Eu sempre limpo a casa.",
+            'respostas': ['I always clean the house.']
+        },
+        {
+            'pergunta': "Eu limpei a casa esta manhã.",
+            'respostas': ['I cleaned the house this morning.']
+        },
+        {
+            'pergunta': "Eu às vezes cozinho carne.",
+            'respostas': ['I sometimes cook meat.']
+        },
+        {
+            'pergunta': "Eu cozinhei peixe ontem à noite.",
+            'respostas': ['I cooked fish last night.']
+        },
+        {
+            'pergunta': "Eu quero caminhar até a praia.",
+            'respostas': ['I want to walk to the beach.']
+        },
+        {
+            'pergunta': "Eu queria caminhar até o Maracanã.",
+            'respostas': ['I wanted to walk to Maracanã.']
+        },
+        {
+            'pergunta': "Eu preciso de você.",
+            'respostas': ['I need you.']
+        },
+        {
+            'pergunta': "Eu precisei de você.",
+            'respostas': ['I needed you.']
+        },
+        {
+            'pergunta': "Eu ligo de manhã.",
+            'respostas': ['I call in the morning.']
+        },
+        {
+            'pergunta': "Eu liguei à tarde.",
+            'respostas': ['I called in the afternoon.']
+        },
+        {
+            'pergunta': "Eu caminho até o rio.",
+            'respostas': ['I walk to the river.']
+        },
+        {
+            'pergunta': "Eu caminhei até a ponte.",
+            'respostas': ['I walked to the bridge.']
+        }
+    ]
+    }     
+}       
